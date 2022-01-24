e56e473286fd6cfccecfe7a8848e4fe5376ad092
zgrkbr <ozgurkibar78@gmail.com>
2022-01-24 14:40:00 +0100

    add dao interface and impl class and necessary deps

---
+++ stidepinj-scratch-dao-impl/src/main/java/stidepinj/scratch/dao/ScratchDaoImpl.java
@@ -0,0 +1,15 @@
+package stidepinj.scratch.dao;
+
+import stidepinj.scratch.domain.Student;
+
+import java.sql.PreparedStatement;
+
+public class ScratchDaoImpl implements ScratchDao{
+
+    PreparedStatement preparedStatement = null;
+
+    @Override
+    public Student addStudent(String givenName, String surname) {
+        return null;
+    }
+}
---
+++ stidepinj-scratch-dao/src/main/java/stidepinj/scratch/dao/ScratchDao.java
@@ -0,0 +1,9 @@
+package stidepinj.scratch.dao;
+
+import stidepinj.scratch.domain.Student;
+
+public interface ScratchDao {
+
+    Student addStudent(String givenName, String surname);
+
+}
