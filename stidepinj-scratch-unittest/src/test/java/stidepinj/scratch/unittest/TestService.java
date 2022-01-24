9cb12253d66bf5b3848da591db5db4f7c41e18db
zgrkbr <ozgurkibar78@gmail.com>
2022-01-24 13:40:17 +0100

    change spring config xml wiring to ServiceGivenName and write unit test

--- stidepinj-scratch-unittest/src/test/java/stidepinj/scratch/unittest/TestService.java
+++ stidepinj-scratch-unittest/src/test/java/stidepinj/scratch/unittest/TestService.java
@@ -1,12 +1,36 @@
 package stidepinj.scratch.unittest;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import stidepinj.scratch.service.ScratchService;
+
+import java.util.List;
 
 public class TestService {
+
+    private static final String HELENA_OSTLUND = "Helena Östlund";
+    private static final String MAX = "Max";
 
     ApplicationContext applicationContext =
             new ClassPathXmlApplicationContext("classpath:stidepinj-service.xml");
 
 
+/*
+    @Test
+    public void testSpringWiring(){
+        ScratchService scratchService = (ScratchService)applicationContext.getBean("service");
+        List<String> students = scratchService.getStudents();
+        Assert.assertEquals(HELENA_OSTLUND, students.get(0));
+    }
+*/
+
+    @Test
+    public void testSpringWiring(){
+        ScratchService scratchService = (ScratchService)applicationContext.getBean("service");
+        List<String> students = scratchService.getStudents();
+        Assert.assertEquals(MAX, students.get(1));
+    }
+    
 }
